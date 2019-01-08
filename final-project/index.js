c8f0c5afc2f1f7ca5ef12f725b4f814734a2e384
Ashlyn Hunt <aahunt92@gmail.com>
2019-01-07 20:42:28 -0600

    footer

---
+++ final-project/index.js
@@ -0,0 +1,5 @@
+import Content from './src/Content';
+import Footer from './src/Footer';
+import Navigation from './src/Navigation';
+import Header from './src/Header';
+
---
+++ final-project/pages/Bracelets.js
@@ -0,0 +1,1 @@
+export default function Bracelets(state){}
