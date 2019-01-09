e0940788b4661c9b1b5829f0a8661d0e520310ec
Ashlyn Hunt <aahunt92@gmail.com>
2019-01-08 20:36:36 -0600

    js pages

---
+++ final-project/pages/index.js
@@ -0,0 +1,5 @@
+export { default as AboutUs } from './About Us';
+export { default as Bracelets } from './Bracelets';
+export { default as Home } from './Home';
+export { default as Contact } from './Contact';
+export { default as FortheKids } from './FortheKids';
---
+++ final-project/src/Content.js
@@ -0,0 +1,10 @@
+export default function Content(state) {
+    return`
+    <div id="content">
+<div class="container">
+    <p></p>
+    
+</div>
+</div>
+`;
+}
