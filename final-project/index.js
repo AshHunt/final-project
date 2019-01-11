85e6760b4255a0a221f73d181bb1c7d9bef23c97
Ashlyn Hunt <aahunt92@gmail.com>
2019-01-10 20:55:10 -0600

    css and some js

--- final-project/index.js
+++ final-project/index.js
@@ -3,3 +3,23 @@ import Footer from './src/Footer';
 import Navigation from './src/Navigation';
 import Header from './src/Header';
 
+var State = {
+    'posts': [],
+    'active': 'home',
+    'home': {
+        'title': 'Loving Lucille Jewelry',
+        'links': ['about', 'contact us', 'bracelets', 'necklaces','keychains']
+    },
+    'blog': {
+        'title': 'Read my amazing blog',
+        'links': ['home', 'contact', 'projects']
+    },
+    'contact': {
+        'title': 'Call me here',
+        'links': ['home', 'blog', 'projects']
+    },
+    'projects': {
+        'title': 'My great work',
+        'links': ['home', 'blog', 'contact']
+    }
+};
